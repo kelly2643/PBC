e9014b4929afbdafc23091284bbe3bbf8ff80f7f
kelly2643 <r08625027@ntu.edu.tw>
2020-12-09 18:44:28 +0800

    Create hw0.py
    test

---
+++ hw0.py
@@ -0,0 +1,8 @@
+x1 = int(input())
+x2 = int(input())
+p1 = int(input())
+p2 = int(input())
+t = int(input())
+total_cost = x1*p1 + x2*p2
+change = t - total_cost
+print(t, total_cost, change, sep=',')
